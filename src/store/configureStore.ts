ccd9b3ca5740f9d90c7ef7520c5117b01c88535f
hyebin829 <smilerain779@gmail.com>
2022-10-07 10:15:56 +0900

    devtools setting

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -3,6 +3,7 @@ import rootReducer from 'reducers'
 
 export const store = configureStore({
   reducer: rootReducer,
+  devTools: process.env.NODE_ENV === 'development',
 })
 
 export type RootState = ReturnType<typeof store.getState>
