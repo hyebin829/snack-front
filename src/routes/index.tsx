38db3980ccf59cafe3c717b7850d65b69bfc18f3
hyebin829 <smilerain779@gmail.com>
2022-10-10 13:07:50 +0900

    test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,43 +1,37 @@
-// import { lazy, Suspense } from 'react'
+import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from 'components/Layout'
 import Home from './Home'
 import NotFound from './NotFoundPage'
-// import LoadingPage from './LoadingPage'
+import LoadingPage from './LoadingPage'
 import styles from './app.module.scss'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import ProfilePage from './ProfilePage'
-import EditProfilePage from './EditProfilePage'
-import MyFavoritePage from './MyFavoritePage'
-import MyReviewPage from './MyReviewPage'
-import SearchPage from './SearchPage'
-import SnackPage from './SnackPage'
 
-// const ProfilePage = lazy(() => import('./ProfilePage'))
-// const EditProfilePage = lazy(() => import('./EditProfilePage'))
-// const MyFavoritePage = lazy(() => import('./MyFavoritePage'))
-// const MyReviewPage = lazy(() => import('./MyReviewPage'))
-// const SearchPage = lazy(() => import('./SearchPage'))
-// const SnackPage = lazy(() => import('./SnackPage'))
+const ProfilePage = lazy(() => import('./ProfilePage'))
+const EditProfilePage = lazy(() => import('./EditProfilePage'))
+const MyFavoritePage = lazy(() => import('./MyFavoritePage'))
+const MyReviewPage = lazy(() => import('./MyReviewPage'))
+const SearchPage = lazy(() => import('./SearchPage'))
+const SnackPage = lazy(() => import('./SnackPage'))
 
 const App = () => {
   return (
     <div className={styles.app}>
-      {/* <Suspense fallback={<LoadingPage />}> */}
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/profile' element={<ProfilePage />} />
-          <Route path='/editprofile' element={<EditProfilePage />} />
-          <Route path='/myfavorite' element={<MyFavoritePage />} />
-          <Route path='/myreview' element={<MyReviewPage />} />
-          <Route path='/search' element={<SearchPage />} />
-          <Route path='/snack/:id' element={<SnackPage />} />
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
-      {/* </Suspense> */}
+      <Suspense fallback={<LoadingPage />}>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/profile' element={<ProfilePage />} />
+            <Route path='/editprofile' element={<EditProfilePage />} />
+            <Route path='/myfavorite' element={<MyFavoritePage />} />
+            <Route path='/myreview' element={<MyReviewPage />} />
+            <Route path='/search' element={<SearchPage />} />
+            <Route path='/snack/:id' element={<SnackPage />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   )
 }
